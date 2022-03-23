5bbb42bfb80d0de3df900e1117eebabdf3cd2a68
Guilherme <gba.itanhaem@gmail.com>
2022-03-23 14:52:33 -0300

    Otimização banner e linkagem

--- assets/js/banners.js
+++ assets/js/banners.js
@@ -54,44 +54,52 @@ var ts_x;
 var ts_y;
 
 banners.addEventListener('touchstart', function(e) {
+ 
+  e.preventDefault();
+  var touch = e.changedTouches[0];
+  ts_x = touch.pageX;
+  ts_y = touch.pageY;
+  
+}, false);
+
+banners.addEventListener('touchend', function(e) {
+
   if(e.target.className=='banner__botao botao botao-ciano'){
     var enderecoLocal = e.target.firstElementChild.textContent;
     console.log(enderecoLocal);
     local = document.querySelector(enderecoLocal);
     local.scrollIntoView();
-  }else{
+  }
+  else{
     e.preventDefault();
     var touch = e.changedTouches[0];
-    ts_x = touch.pageX;
-    ts_y = touch.pageY;
+    td_x = touch.pageX - ts_x; // deslocamento na horizontal
+    td_y = touch.pageY - ts_y; // deslocamento na vertical
+    // O movimento principal foi vertical ou horizontal?
+    if( Math.max( Math.abs( td_x )) > 100 ) {
+        // é horizontal
+        if( td_x < 10 ) {
+          mudarAutomatico();
+        } else if(td_x > 10 ){
+          voltarBanner();
+        }
+        else{
+          alert('teste');
+        }
+    }
+    else if( Math.abs(td_y) > 10 ) {
+        // é vertical
+        if( td_y < 0 ) {
+          // cima
+          var valorScroll = scrollY + touch.pageY;
+          console.log(valorScroll);
+          window.scrollTo(0,valorScroll);
+        } else {
+          // baixo
+          window.scrollTo(0,0);
+        }
+    }
   }
-}, false);
 
-banners.addEventListener('touchend', function(e) {
-  e.preventDefault();
-  var touch = e.changedTouches[0];
-  td_x = touch.pageX - ts_x; // deslocamento na horizontal
-  td_y = touch.pageY - ts_y; // deslocamento na vertical
-  // O movimento principal foi vertical ou horizontal?
-  if( Math.max( Math.abs( td_x )) > 100 ) {
-      // é horizontal
-      if( td_x < 10 ) {
-        mudarAutomatico();
-      } else if(td_x > 10 ){
-        voltarBanner();
-      }
-      else{
-        alert('teste');
-      }
-  }
-  else if( Math.abs(td_y) > 10 ) {
-      // é vertical
-      if( td_y < 0 ) {
-        // cima
-        window.scrollTo(0,200);
-      } else {
-        // baixo
-        window.scrollTo(0,0);
-      }
-  }
+  
 }, false);
